565e657c96e98eda0bd37b80022ad75def69cae0
Kamaldeep Srinath <kasrinat@akamai.com>
2016-11-14 19:04:33 +0530

    Initialize the class variable to prevent undefined access following try/catch in __init__. Change for review.

--- tidylib/tidy.py
+++ tidylib/tidy.py
@@ -81,6 +81,9 @@ class Tidy(object):
     """ Wrapper around the HTML Tidy library for cleaning up possibly invalid
     HTML and XHTML. """
 
+    # Initialize the class variable.
+    _tidy = None
+
     def __init__(self, lib_names=None):
         if lib_names is None:
             lib_names = ctypes.util.find_library('tidy') or LIB_NAMES
